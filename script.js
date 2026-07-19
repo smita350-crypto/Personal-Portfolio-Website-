fbeca423b240ef38718c0fcf7e0c95b2bd308d5c
Smita Powar <smitapowar678@gmail.com>
2026-07-19 22:02:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-// Welcome Message
-console.log("Welcome to Smita Powar Portfolio");
+// Dark Mode
 
-// Smooth Scrolling for Navigation
-const links = document.querySelectorAll("nav a");
+const darkBtn = document.getElementById("darkModeBtn");
 
-links.forEach(link => {
-    link.addEventListener("click", function(e) {
-        e.preventDefault();
+darkBtn.onclick = function () {
+    document.body.classList.toggle("dark-mode");
 
-        const targetId = this.getAttribute("href");
-        const targetSection = document.querySelector(targetId);
+    if(document.body.classList.contains("dark-mode")){
+        darkBtn.innerHTML="☀ Light Mode";
+    }else{
+        darkBtn.innerHTML="🌙 Dark Mode";
+    }
+};
 
-        targetSection.scrollIntoView({
-            behavior: "smooth"
-        });
+
+// Scroll To Top Button
+
+const topBtn=document.getElementById("topBtn");
+
+window.onscroll=function(){
+
+    if(document.body.scrollTop>200 || document.documentElement.scrollTop>200){
+        topBtn.style.display="block";
+    }else{
+        topBtn.style.display="none";
+    }
+
+};
+
+topBtn.onclick=function(){
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
     });
-});
 
-// Highlight Navigation Link
-links.forEach(link => {
-    link.addEventListener("mouseenter", () => {
-        link.style.color = "#0d6efd";
-    });
-
-    link.addEventListener("mouseleave", () => {
-        link.style.color = "white";
-    });
-});
-
-// Footer Year
-const footer = document.querySelector("footer p");
-footer.innerHTML = `© ${new Date().getFullYear()} Smita Powar. All Rights Reserved.`;
+};
