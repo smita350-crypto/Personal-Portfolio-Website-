114ea850dc0d475535884d104a43416705d77c84
Smita Powar <smitapowar678@gmail.com>
2026-08-29 21:52:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,11 @@
-// Dark Mode
+const theme = document.getElementById("theme");
 
-const darkBtn = document.getElementById("darkModeBtn");
+theme.onclick = function () {
+    document.body.classList.toggle("dark");
 
-darkBtn.onclick = function () {
-    document.body.classList.toggle("dark-mode");
-
-    if(document.body.classList.contains("dark-mode")){
-        darkBtn.innerHTML="☀ Light Mode";
-    }else{
-        darkBtn.innerHTML="🌙 Dark Mode";
+    if (document.body.classList.contains("dark")) {
+        theme.innerHTML = "☀️";
+    } else {
+        theme.innerHTML = "🌙";
     }
 };
-
-
-// Scroll To Top Button
-
-const topBtn=document.getElementById("topBtn");
-
-window.onscroll=function(){
-
-    if(document.body.scrollTop>200 || document.documentElement.scrollTop>200){
-        topBtn.style.display="block";
-    }else{
-        topBtn.style.display="none";
-    }
-
-};
-
-topBtn.onclick=function(){
-
-    window.scrollTo({
-        top:0,
-        behavior:"smooth"
-    });
-
-};
-// Contact Form Validation
-
-const form = document.getElementById("contactForm");
-
-form.addEventListener("submit", function(e){
-
-    e.preventDefault();
-
-    const name = form.querySelector('input[type="text"]').value.trim();
-
-    const email = form.querySelector('input[type="email"]').value.trim();
-
-    const message = form.querySelector("textarea").value.trim();
-
-    if(name==="" || email==="" || message===""){
-        alert("Please fill all fields.");
-        return;
-    }
-
-    alert("Message Sent Successfully!");
-
-    form.reset();
-
-});
